821a8d0b253a439422aa72e77bd07fd9a442c267
voiceless-zell <peter.bouchard2893@proton.me>
2024-07-11 10:19:32 -0400

    monitor/ zsh updates

--- hosts/P16/homemanager.nix
+++ hosts/P16/homemanager.nix
@@ -3,9 +3,9 @@
    home-manager.users.zell = { 
     wayland.windowManager.hyprland.settings.monitor = [
      "eDP-1,2560x1600,0x0,1"
-#     "DP-3,1920x1080,0x-1080, 1"
-     "HDMI-A-1,1920x1080,-1920x0, 1"
-#     "DP-2,1920x1080,-3840x-1080,1"
+     "DP-3,1920x1080,-5760x0, 1"
+     "HDMI-A-1,1920x1080,-3840x0, 1"
+     "DP-2,1920x1080,-x-1080,1"
       ]; 
 #    wayland.windowManager.hyprland.settings.workspace = [
 #    "1,name:Terminal,on-created-empty:foot -e tmux,monitor:HDMI-A-1"
--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -30,10 +30,10 @@ in {
       v = "nvim";
       record = "wf-recorder --audio=alsa_output.pci-0000_08_00.6.analog-stereo.monitor -f $HOME/Videos/$(date +'%Y%m%d%H%M%S_1.mp4')";
       ls = "eza --icons --long";
-      startbg = " nohup ~/flake/backgrounds/swww_randomize.sh ~/flake/backgrounds > output.log 2>&1 &";
+      startbg = " nohup ~/flake2/backgrounds/swww_randomize.sh ~/flake2/backgrounds > output.log 2>&1 &";
       # nixos
       fl = "cd ~/flake2/ && v";
-      notes = "cd ~/notes/ && v";
+      notes = "cd ~/Documents/Master && v";
       music = "ncspot";
       vm = "sudo virsh net-start default";
     };
